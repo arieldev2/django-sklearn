de7e3c965088217297c1c8e9fb213389d9f51383
ariel <llamas0123@hotmail.com>
2019-03-15 00:04:43 -0700

    cuarto commit

--- ml_project/settings.py
+++ ml_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['machinelearningdjango.herokuapp.com']
+ALLOWED_HOSTS = ['djangomlapp.herokuapp.com']
 
 
 # Application definition
